e1b00e5195fd03b506101493ba05500e51e1654c
Hack Reactor Student <student@hackreactor.com>
2017-06-28 12:30:05 -0500

    added prototypal queue, passes tests

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var newQueue = Object.create(queueMethods);
+
+  newQueue.storage = {};
+  newQueue.length = 0;
+  newQueue.start = 0;
+  newQueue.end = 0;
+  
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[end] = value;
+  this.length++;
+};
 
+queueMethods.dequeue = function() {
+  if (this.length > 0) {
+    var out = this.storage[start];
+    this.storage[start] = undefined;
+    this.start++;
+    this.length--;
+    return out;
+  }
+};
+
+queueMethods.size = function() {
+  return this.length;
+};
