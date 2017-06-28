5c64567fc306bf6663db59ae3ffd90e681790340
Hack Reactor Student <student@hackreactor.com>
2017-06-28 16:01:07 -0500

    tree.js complete, all specs pass

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -15,11 +15,26 @@ treeMethods.addChild = function(value) {
   //create a childTree(value)
   //add this new child as a child of the current tree
   var childTree = new Tree(value);
+  this.children.push(childTree);
   
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, out) {
   //recursively goes through child nodes until a result is found or there are no more children
+  var out = out || false;
+  //returns true or false
+  
+  if (this.value === target) { 
+    return true;
+  } else {
+    if (!out && this.children.length > 0) { //if out is false and there are children
+      for (var i = 0; i < this.children.length; i++ ) {
+        out = this.children[i].contains(target, out);
+      }
+    }
+  }
+  
+  return out;
 };
 
 
