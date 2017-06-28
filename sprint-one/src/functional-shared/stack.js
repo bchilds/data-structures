7a1a91ba65aac2a05bb540651b9ffab79902bc04
Hack Reactor Student <student@hackreactor.com>
2017-06-28 11:09:55 -0500

    initial framework for functional shared methods stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var storage = {};
+  var size = 0;
+  extend(this, stackMethods);
 };
 
 var stackMethods = {};
 
+stackMethods.push = function() {
 
+};
+
+stackMethods.pop = function() {
+
+};
+
+stackMethods.size = function() {
+
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
