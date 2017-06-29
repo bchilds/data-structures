3823a5f7a68b065efa32c7da198b2b34b6eacc70
Hack Reactor Student <student@hackreactor.com>
2017-06-29 11:30:44 -0500

    outline of BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,31 @@
 var BinarySearchTree = function(value) {
+
+  var bst = {};
+  bst.value = value;
+  bst.left = {};
+  bst.right = {};
+  _.extend(bst, bstMethods);
+  return bst;
 };
 
+var bstMethods = {};
+//will need to access values recursively for correct placement/location
+bstMethods.insert = function(value) {
+  
 
+};
+
+bstMethods.contains = function(value) {
+  //returns boolean if BST contains value
+  var out;
+
+  return out;
+};
+
+bstMethods.depthFirstLog = function(callback) {
+  //executed callback function on each element in BST
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
