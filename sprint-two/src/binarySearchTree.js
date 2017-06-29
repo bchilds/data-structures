516327b5a0bcace65951562536940b66b95c84bb
Hack Reactor Student <student@hackreactor.com>
2017-06-29 14:27:02 -0500

    BST insert working

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -12,14 +12,20 @@ var bstMethods = {};
 //will need to access values recursively for correct placement/location
 bstMethods.insert = function(value) {
   //compare value to this BST value. 
-    //if lower, 
-      //call insert on this.left if left exists
-      //create a new BST with value and point this.left to it if this.left does not exist
-    //if higher, 
-      //call insert on this.right if right exists
-      //create a new BST with value and point this.right to it if this.right does not exist
-      
   
+  //if lower, 
+  if (value < this.value) {
+    (this.left !== undefined) ? this.left.insert(value) : (this.left = new BinarySearchTree(value));
+    //call insert on this.left if left exists
+    //create a new BST with value and point this.left to it if this.left does not exist
+  //if higher,
+  } else if (value > this.value) {
+    (this.right !== undefined) ? this.right.insert(value) : (this.right = new BinarySearchTree(value));
+    //call insert on this.right if right exists
+    //create a new BST with value and point this.right to it if this.right does not exist
+  } else if (value === this.value) {
+    //if equal, do nothing
+  }
 
 };
 
