b0d5abf66bab4bf60585a0ff0383a9482b2d4f20
Hack Reactor Student <student@hackreactor.com>
2017-06-30 11:45:05 -0500

    progress on has table resizing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,12 +1,13 @@
 
 
 var HashTable = function() {
-  this._limit = 8;
+  this._limit = 8; //this is the base value
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
   //refactor: always store tuples using insert
+  this.checkUsage();
   var tuple = [k, v];
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage.get(index) === undefined) {
@@ -52,8 +53,62 @@ HashTable.prototype.remove = function(k) {
       break;
     }
   }
+  
+  //set the bucket to undefined again
+  if (this._storage.get(index).length === 0) { this._storage.set(index, undefined); }
 };
 
+HashTable.prototype.rehashAll = function(oldStorage, newStorage) {
+  //should take the current hash table and re-hash it
+  //with the current limit
+
+  //use limitedArray.each to go across each old bucket
+  //rehash each value in those buckets into this._storage
+
+  //oldStorage is class limitedArray
+  //old storage will contain tuples which need to be rehashed
+  
+  oldStorage.each(function(bucketArray) {
+    for (var i = 0; i < bucketArray ? bucketArray.length : 0; i++ ) {
+      //debugger;
+      //newStorage.insert(bucketArray[i][0], bucketArray[i][1]);
+    }
+  });
+};
+
+HashTable.prototype.checkUsage = function() {
+  //checks if the current usage if >75% or <25%
+  //modifies the limit accordingly and rehashes table if so
+
+  //things to keep in mind:
+  //   buckets are limited arrays, not normal arrays
+
+  var indicesUsed, ratio, oldStorage, needsRehash, newStorage;
+  
+  indicesUsed = 0;
+  needsRehash = false;
+
+  this._storage.each(function(index) {
+    if (index) { indicesUsed++; }
+  });
+
+  ratio = indicesUsed / this._limit;
+  if (ratio >= 0.75) {
+    this._limit = this._limit * 2;
+    needsRehash = true;
+  } else if (ratio <= 0.25 && this._limit > 1) {
+    this._limit = this._limit / 2;
+    needsRehash = true;
+  }
+
+  if (needsRehash) { 
+    oldStorage = this._storage;
+    newStorage = LimitedArray(this._limit);
+    this.rehashAll(oldStorage, this);
+    this._storage = newStorage;
+  }
+  
+};
 
 
 /*
